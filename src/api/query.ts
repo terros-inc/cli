9ecc0d8e888085e62b93943f6ea623f4f0367b19
DJ Nelson <dj@terros.com>
2026-06-17 14:08:21 -0700

    feat(query): add request analytics, impersonation, API endpoint override

--- src/api/query.ts
+++ src/api/query.ts
@@ -1,11 +1,18 @@
+import * as process from 'node:process'
 import { getAuthorizationHeader } from './auth.ts'
+import { getAnalyticsHeaders } from './analytics.ts'
 
 export async function queryTerrosAPI(path: string, input: object): Promise<object> {
+  const endpoint = getApiEndpoint()
+  const impersonation = getImpersonationHeaders()
+  const analytics = getAnalyticsHeaders()
   const authorization = await getAuthorizationHeader()
 
-  const res = await fetch(`https://api.terros.com${path}`, {
+  const res = await fetch(`${endpoint}${path}`, {
     method: 'POST',
     headers: {
+      ...analytics,
+      ...impersonation,
       Authorization: authorization,
       'Content-Type': 'application/json',
     },
@@ -13,4 +20,18 @@ export async function queryTerrosAPI(path: string, input: object): Promise<objec
   })
 
   return (await res.json()) as object
+}
+
+function getApiEndpoint(): string {
+  const envEndpoint = process.env.TERROS_API_ENDPOINT
+  if (envEndpoint) return envEndpoint
+
+  return 'https://api.terros.com'
+}
+
+function getImpersonationHeaders(): Record<string, string> {
+  const userId = process.env.TERROS_IMPERSONATE
+  if (userId) return { impersonate_user_id: userId }
+
+  return {}
 }
