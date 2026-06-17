8a5daf010daa8ecf9539e153e888791e72d09d9a
DJ Nelson <dj@terros.com>
2026-06-17 13:25:16 -0700

    chore: add vitest

---
+++ src/crud/input.test.ts
@@ -0,0 +1,39 @@
+// import { expect } from 'vitest'
+// import type { ParsedArgs } from 'minimist'
+// import { buildEndpointInput } from './input.ts'
+// import type { Endpoint } from './endpoint.ts'
+
+describe.skip('sample test', () => {
+  it('should handle params correctly', () => {
+    // const endpoint: Endpoint = {
+    //   path: '/user/profile',
+    //   parameters: [
+    //     {
+    //       name: 'userId',
+    //       type: 'string',
+    //       required: false,
+    //     },
+    //   ],
+    //   properties: {
+    //     type: 'object',
+    //     properties: {
+    //       userId: {
+    //         type: 'string',
+    //       },
+    //     },
+    //   },
+    // }
+    //
+    // const params: ParsedArgs = {
+    //   _: ['user', 'profile'],
+    //   userId: 'U:dj',
+    // }
+    //
+    // const expected = {
+    //   userId: 'U:dj',
+    // }
+    //
+    // const result = buildEndpointInput(endpoint, params)
+    // expect(result).toEqual(expected)
+  })
+})
